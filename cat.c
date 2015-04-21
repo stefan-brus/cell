24034f2f4bc129477c573143f883b8611333a065
Stefan Brus <stefan1brus@gmail.com>
2015-04-21 16:46:51 -0700

    Add -n option to print line numbers to cat

--- cat.c
+++ cat.c
@@ -3,46 +3,95 @@
  */
 
 #include "dbg.h"
+#include <stdbool.h>
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * Echo characters to stdout from the given file until EOF
  *
  * Args:
  *      f = The file
+ *      lines = Whether or not to print line numbers
  */
 
-void echo(FILE* f)
+void echo(FILE* f, bool lines)
 {
     int c = 0;
+    int line = 1;
+    bool print_line = true;
 
     check(f, "Can't cat unopened file.");
 
     while((c = fgetc(f)) != EOF)
     {
+        if(lines && print_line)
+        {
+            printf("%d: ", line);
+            print_line = false;
+        }
+
         fputc(c, stdout);
+
+        if(c == '\n')
+        {
+            line++;
+            print_line = true;
+        }
     }
 
 error:
     return;
 }
 
+/**
+ * Read the command line options from argv
+ * Write command line options into the output arguments
+ *
+ * Args:
+ *      argc = argument count
+ *      argv = argument vector
+ *      n = output, whether or not 'n' was set
+ *
+ * TODO: Generalize and make this a vararg function
+ */
+
+void read_opts(int argc, char* argv[], bool* n)
+{
+    int opt = 0;
+
+    while((opt = getopt(argc, argv, "n")) != -1)
+    {
+        switch(opt)
+        {
+            case 'n':
+                *n = true;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
 int main(int argc, char* argv[])
 {
     FILE *f = NULL;
     int i = 0;
+    bool n = false;
 
-    if(argc == 1)
+    read_opts(argc, argv, &n);
+
+    if(argc == 1 || optind >= argc)
     {
-        echo(stdin);
+        echo(stdin, n);
     }
     else
     {
-        for(i = 1; i < argc; i++)
+        for(i = optind; i < argc; i++)
         {
             f = fopen(argv[i], "r");
             check(f, "Error opening file: %s", argv[i]);
-            echo(f);
+            echo(f, n);
             fclose(f);
         }
     }

76386af8f8972986dbb10526a6312b68ac1f3338
Stefan Brus <stefan1brus@gmail.com>
2015-04-21 16:04:01 -0700

    Cat can handle arbitrary number of files
    Also:
    - Add debug macros

--- cat.c
+++ cat.c
@@ -2,16 +2,58 @@
  * cat command
  */
 
+#include "dbg.h"
 #include <stdio.h>
 
-int main(int argc, char* argv[])
+/**
+ * Echo characters to stdout from the given file until EOF
+ *
+ * Args:
+ *      f = The file
+ */
+
+void echo(FILE* f)
 {
     int c;
 
-    while((c = fgetc(stdin)) != EOF)
+    check(f, "Can't cat unopened file.");
+
+    while((c = fgetc(f)) != EOF)
     {
         fputc(c, stdout);
     }
 
+error:
+    return;
+}
+
+int main(int argc, char* argv[])
+{
+    FILE *f = NULL;
+    int i = 0;
+
+    if(argc == 1)
+    {
+        echo(stdin);
+    }
+    else
+    {
+        for(i = 1; i < argc; i++)
+        {
+            f = fopen(argv[i], "r");
+            check(f, "Error opening file: %s", argv[i]);
+            echo(f);
+            fclose(f);
+        }
+    }
+
     return 0;
+
+error:
+    if(f)
+    {
+        fclose(f);
+    }
+
+    return 1;
 }

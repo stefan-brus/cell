0b49fcddc19b049a1a17df200e082c27f229da67
Stefan Brus <stefan1brus@gmail.com>
2015-04-23 02:36:02 -0700

    Use scandir instead of readdir, filter dots

--- ls.c
+++ ls.c
@@ -3,35 +3,84 @@
  *
  * TODO:
  *      - Add options
- *      - Sort output
+ *      - Sort output better, ignore case
  *      - Handle path argument
  */
 
 #include "dbg.h"
 #include <dirent.h>
-#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+
+/**
+ * Default path to open as a directory
+ */
+
+const char* DEFAULT_PATH = ".";
+
+/**
+ * Free the given directory entries
+ *
+ * ARGS:
+ *      count = The number of entries to free
+ *      entries = The entries
+ */
+
+void free_entries(int count, struct dirent** entries)
+{
+    int i = 0;
+
+    for(i = 0; i < count; i++)
+    {
+        free(entries[i]);
+    }
+
+    free(entries);
+}
+
+/**
+ * Filter dot entries, passed to scandir's filter argument
+ *
+ * ARGS:
+ *      entry = The directory entry
+ *
+ * RETURNS:
+ *      0 if entry begins with a dot, 1 otherwise
+ */
+
+int filter_dot(const struct dirent* entry)
+{
+    check(entry, "Cannot filter null entry");
+
+    return entry->d_name[0] != '.';
+
+error:
+    return 0;
+}
 
 int main(int argc, char* argv[])
 {
-    DIR* d = NULL;
-    struct dirent* dir = NULL;
+    struct dirent** entries = NULL;
+    int rc = 0;
+    int i = 0;
 
-    d = opendir(".");
-    check(d, "Unable to open directory: %s", ".");
+    rc = scandir(DEFAULT_PATH, &entries, filter_dot, alphasort);
+    check(rc >= 0, "Unable to open directory: %s", DEFAULT_PATH);
 
-    while((dir = readdir(d)))
+    for(i = 0; i < rc; i++)
     {
-        printf("%s\n", dir->d_name);
+        printf("%s\n", entries[i]->d_name);
     }
+
+    free_entries(rc, entries);
 
     return 0;
 
 error:
-    if(d)
+    if(rc > 0 && entries)
     {
-        closedir(d);
+        free_entries(rc, entries);
     }
 
     return 1;
